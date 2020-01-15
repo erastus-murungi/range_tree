88c3cb8467654a30d29bb13cac5a58ce1d47c95e
Erastus Murungi <erastuswagwan@gmail.com>
2020-01-14 20:31:28 -0500

    Update range_tree.py

--- range_tree.py
+++ range_tree.py
@@ -33,7 +33,7 @@ def max_left_subtree(node):
 def range_tree(values):
     """Build a 1D Range Tree and returns the root, and the nodes on the same level"""
     assert (len(values)), "Empty iterable."
-    leaves = list(map(lambda val: Leaf(val, None), values))
+    leaves = list(map(lambda val: Leaf(val, None), sorted(values)))
     if len(leaves) == 1:
         return Leaf(values[0], None)
 
@@ -43,6 +43,7 @@ def range_tree(values):
         for i in range(1, n, 2):
             l, r = leaves[i - 1], leaves[i]
             x = Node(l, r, max_left_subtree(l), None)
+            l.parent = r.parent = x
             nodes.append(x)
         if n & 1:  # if odd
             x = Node(leaves[n - 1], None, max_left_subtree(leaves[n - 1]), None)
@@ -54,50 +55,83 @@ def range_tree(values):
     return levels[-1][0], levels
 
 
-def access(subtree_root, key):
-    assert subtree_root, "Null tree"
-    while not isleaf(subtree_root):
-        subtree_root = subtree_root.right if key >= subtree_root.key else subtree_root.left
-    return subtree_root
+def report(output: list):
+    def __report_helper(node):
+        if isleaf(node.left):
+            yield node.left.key
+        else:
+            yield from node.left.key
+        yield node.key
+        if isleaf(node.right):
+            yield node.right.key
+        else:
+            yield from node.right
+
+    for node in output:
+        if isleaf(node):
+            yield node.key
+        else:
+            yield from __report_helper(node)
 
 
-def lca(root, node1, node2):
-    pass
+def find_split_node(root, x, y):
+    """ Finds and returns the split node
+        For the range query [x : y], the node v in a balanced binary search
+        tree is a split node if its value x.v satisfies x.v ≥ x and x.v < x """
+
+    v = root
+    while not isleaf(v) and (v.key >= y or v.key < x):
+        v = v.left if y <= v.key else v.right
+    return v
 
 
 def query_range_tree(root, i, j):
+    """Querying a 1D Range Tree."""
     if i > j:
         i, j = j, i
-    node_i, node_j = access(root, i), access(root, j)
-    assert node_j != node_i
+
     output = []
-    common = lca(root, node_i, node_j)
-    # query left
-    if node_i.key == i:
-        output.append(node_i)
-    node_i = node_i.parent
-    while node_i is not common and node_i is not root:
-        if node_i is node_i.parent.left:
-            output.append(node_i.right)
-            node_i = node_i.parent
-    output.append(common)
-    # query right
-    if node_j.key == j:
-        output.append(node_j)
-    node_j = node_j.parent
-    while node_j is not common and node_j is not root:
-        if node_j is node_j.parent.right:
-            output.append(node_j.left)
-            node_j = node_j.parent
+    v_split = find_split_node(root, i, j)
+    if isleaf(v_split):
+        # check if the key in v_split
+        if i <= v_split.key <= j:  # inclusive version
+            output.append(v_split)
+    else:
+        v = v_split.left
+        while not isleaf(v):
+            if i <= v.key:
+                # report right subtree
+                output.append(v.right)
+                v = v.left
+            else:
+                v = v.right
+        # v is now a leaf
+        if v.key >= i:
+            output.append(v)
+        # now we follow right side
+        v = v_split.right
+        while not isleaf(v):
+            if v.key < j:
+                # report left subtree
+                output.append(v.left)
+                v = v.right
+            else:
+                v = v.left
     return output
 
 
 if __name__ == '__main__':
     from pprint import pprint
-    points = [6, 15, 17, 21, 24, 33, 42, 51, 52, 57, 65, 73, 78]
-    rtree, levs = range_tree(points)
-    vals = [list(map(lambda x: x.key, values)) for values in levs]
+    points = [3, 10, 19, 23, 30, 37, 59, 62, 70, 80, 100, 105]
+    rtree, levels_ = range_tree(points)
+    vals = [list(map(lambda x: x.key, values)) for values in levels_]
     vals.reverse()
     pprint(vals)
 
-    print(query_range_tree(rtree, 15, 51))
+    rep = query_range_tree(rtree, 20, 60)
+    gen = report(rep)
+    try:
+        while True:
+            print(next(gen))
+    except StopIteration:
+        pass
