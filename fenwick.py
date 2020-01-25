e0710b6678ce31525e70e9688a62fba9a6a15198
Erastus Murungi <erastuswagwan@gmail.com>
2020-01-25 10:36:25 -0500

    Update fenwick.py

--- fenwick.py
+++ fenwick.py
@@ -6,9 +6,9 @@ def lsb(x):
 
 
 class FenwickTree:
-    """I-D Fenwick tree"""
+    """I-D Fenwick tree."""
     def __init__(self, arr):
-        self.array = np.array([0] + arr)
+        self.array = np.insert(arr, 0, 0)
         self.construct()
 
     def construct(self):
@@ -20,18 +20,16 @@ class FenwickTree:
             if j < n:
                 self.array[j] += self.array[i]
 
-    def update(self, i, val):
-        diff = self.array[i] - val
-
-        self.array[i] = val
+    def update(self, i, new_val):
+        # ass
+        diff = self.array[i] - new_val
+        self.array[i] = new_val
         j = i + lsb(i)
-
         while j < len(self.array):
             self.array[j] += diff
             j = i + lsb(i)
 
     def point_query(self, i):
-
         i += 1  # because of 1-based indexing
         total = 0
         while i:
@@ -48,8 +46,16 @@ class FenwickTree:
 
 if __name__ == '__main__':
     from random import randint
-    nums = [randint(0, 100) for _ in range(100)]
-    print(nums)
+    from time import perf_counter
+    nums = np.random.choice(100_000_000, 10_000_000)
+    t1 = perf_counter()
     f_tree = FenwickTree(nums)
-    y = f_tree.point_query(2)
-    print(y
+    print(f"Fenwick tree constructed in {perf_counter() - t1} seconds.")
+    t2 = perf_counter()
+    y = f_tree.point_query(3_000_000)
+    print(f"Fenwick tree queried in {perf_counter() - t2} seconds.")
+    t3 = perf_counter()
+    z = np.sum(nums[:3_000_001])
+    print(f"Numpy sum done in {perf_counter() - t3} seconds.")
+
+    print(y, z)
